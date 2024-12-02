2b609390f405e8a344b74258e7db9bc02a7ca6da
Brayan <brayanfernandezjimenez18@gmail.com>
2024-12-02 10:57:34 -0500

    desarrollo ejercicio 5

---
+++ JS/ejercicio_5.js
@@ -0,0 +1,5 @@
+let edad=prompt("Por favor ingrese su edad: ");
+let ingresos=prompt("Por favor ingrese el valor de sus ingresos ")
+
+if(edad>16 && ingresos>=1000)console.log("El usuario debera pagar el impuesto");
+else console.log("El usuario no debera pagar el impuesto.")
