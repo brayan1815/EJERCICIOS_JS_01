61f1af6caa82e69c0745577039aec33ebf8474bb
Brayan <brayanfernandezjimenez18@gmail.com>
2024-12-07 12:17:28 -0500

    desarrollo ejercicio 45.

---
+++ JS/ejercicio_45.js
@@ -0,0 +1,9 @@
+let num=parseInt(prompt("Ingrese un numero: "));
+
+if(num>0 && num<100){
+    alert("El numero es positivo y menor que cien");
+}else if(num<0){
+    alert("El numero ingresado es negativo")
+}else{
+    alert("El numero es mayor que Cien");
+}
