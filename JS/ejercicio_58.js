f3fcaa2653df0202af527ed136fceb0b3a8f23b3
Brayan <brayanfernandezjimenez18@gmail.com>
2024-12-08 14:39:18 -0500

    desarrollo ejercicio 58

---
+++ JS/ejercicio_58.js
@@ -0,0 +1,16 @@
+let mes=prompt("Por favor ingrese el mes de nacimiento: ");
+
+mes=mes.toLowerCase();
+
+if (mes === "enero")alert( "Capricornio (1 - 19) / Acuario (20 - 31)");
+else if (mes === "febrero")alert( "Acuario (1 - 18) / Piscis (19 - 29)");
+else if (mes === "marzo") alert( "Piscis (1 - 20) / Aries (21 - 31)");
+else if (mes === "abril") alert( "Aries (1 - 19) / Tauro (20 - 30)");
+else if (mes === "mayo") alert( "Tauro (1 - 20) / Géminis (21 - 31)");
+else if (mes === "junio") alert( "Géminis (1 - 20) / Cáncer (21 - 30)");
+else if (mes === "julio") alert( "Cáncer (1 - 22) / Leo (23 - 31)");
+else if (mes === "agosto") alert( "Leo (1 - 22) / Virgo (23 - 31)");
+else if (mes === "septiembre") alert( "Virgo (1 - 22) / Libra (23 - 30)");
+else if (mes === "octubre") alert( "Libra (1 - 22) / Escorpio (23 - 31)");
+else if (mes === "noviembre") alert( "Escorpio (1 - 21) / Sagitario (22 - 30)");
+else if (mes === "diciembre") alert( "Sagitario (1 - 21) / Capricornio (22 - 31)");
