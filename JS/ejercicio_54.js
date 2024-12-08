738015ff02ca76f7c080691649c4bb6af0464467
Brayan <brayanfernandezjimenez18@gmail.com>
2024-12-08 14:01:37 -0500

    desarrollo ejercicio 54

---
+++ JS/ejercicio_54.js
@@ -0,0 +1,6 @@
+let num1=parseFloat(prompt("Por favor ingrese el valor del primer numero: "));
+let num2=parseFloat(prompt("Por favor ingrese el valor del segundo numero: "));
+
+if(num1>num2){
+    alert("La resta de los numeros es: "+(num1-num2));
+}else alert("No es posible realizar la resta porque el primer numero es menor que el segundo");
