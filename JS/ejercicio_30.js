7ee0ec16201ad4c76a3c50b96bdbb187a1d3d667
Brayan <brayanfernandezjimenez18@gmail.com>
2024-12-09 14:26:13 -0500

    ajuste ejercicio 30

--- JS/ejercicio_30.js
+++ JS/ejercicio_30.js
@@ -1,5 +1,6 @@
-let contra="BRAyan1525";
+let contra=prompt("Por favor ingrese una contraseña para determinar si es segura o no");
 
 let expre=/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])([^\s]){8,12}/;
 
-console.log(expre.test(contra));
+if(expre.test(contra)) alert("LA contraseña es segura");
+else alert("La contraseña no es segura");
