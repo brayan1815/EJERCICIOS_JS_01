15a3c976ddff77e6ff1d9617fd70ac27ee2ef3c5
Brayan <brayanfernandezjimenez18@gmail.com>
2024-12-09 13:51:08 -0500

    desarrollo ejercicio 31

---
+++ JS/ejercicio_31.js
@@ -0,0 +1,9 @@
+let x1=parseInt(prompt("Por favor ingrese la coordenada x1: "));
+let x2=parseInt(prompt("Por favor ingrese la coordenada x2: "));
+let y1=parseInt(prompt("Por favor ingrese la coordenada y1: "));
+let y2=parseInt(prompt("Por favor ingrese la coordenada y2: "));
+
+let distancia=((x2-x1)**2)+((y2-y1)**2);
+
+alert("La distancia entre los dos puntos es: "+distancia);
+
